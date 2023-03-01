a8336a24abb9b7de526da94a599c031f4bf52984
NickoIlham <95081601+NickoIlham@users.noreply.github.com>
2023-03-01 15:30:58 +0700

    Upload folder assets dan file index.html

---
+++ assets/logic.js
@@ -0,0 +1,8 @@
+function hamburger_button(){
+  var i = document.getElementById("link")
+  if (i.style.display === "block") {
+    i.style.display = "none";
+  } else {
+    i.style.display = "block";
+  }
+}
